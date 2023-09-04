8087c6e8144b96101d49015f0050bcbae2f6ef28
Hepaul7 <paulhe0773@gmail.com>
2023-09-04 23:10:49 +0200

    export to csv ability added

--- scrap.py
+++ scrap.py
@@ -1,15 +1,30 @@
 import requests
+import csv
+import re
+
 from bs4 import BeautifulSoup
 from requests import Response
 from typing import Optional, Set, Dict, List
 from urllib.parse import urljoin
 
 URL = 'https://www.uoftbookstore.com/general-books/genres?page=1'
+PATH = './discounts.csv'
+
+
+def float_price(price: str) -> float:
+    """ Return the price of the given string converted as float"""
+
+    matches = re.findall(r"[-+]?\d*\.\d+|\d+", price)
+
+    if matches:
+        return float(matches[0])
 
 
 class WebScrapper:
-    """ Class for
-
+    """ Class for a webscrapper
+        self.url : the url of the website
+        self.response : stores the response of the request sent
+        self.soup : the bs4 object
     """
     def __init__(self, url) -> None:
         self.url = url
@@ -78,25 +93,32 @@ class WebScrapper:
 
 
 class Product(WebScrapper):
+    """ A class for the product, inherited from WebScrapper,
+    storing additional information of its current price and old price
+    if there is a discount.
+    """
 
     def __init__(self, url):
         super().__init__(url)
         self.curr_price = None
         self.old_price = None
 
-    def set_curr_price(self):
+    def set_curr_price(self, price_class):
+        """ Each product is assumed to have a price value
+        :return:
+        """
         soup = self.check_soup()
         if not soup:
             return False
 
-        self.curr_price = soup.find(class_='product-views-price-lead')
+        self.curr_price = float_price(soup.find(class_=price_class).text)
 
-    def set_old_price(self):
+    def set_old_price(self, old_price_class):
         soup = self.check_soup()
         if not soup:
             return False
 
-        self.old_price = soup.find(class_='product-views-price-old')
+        self.old_price = float_price(soup.find(class_=old_price_class).text)
 
 
 def get_all_products(scrapper: WebScrapper) -> Optional[Set[str]]:
@@ -116,27 +138,40 @@ def get_all_products(scrapper: WebScrapper) -> Optional[Set[str]]:
     return products
 
 
-def check_discount(products) -> Optional[Dict]:
+def check_discount(products: Set[str]) -> Dict:
     """ For each of the products, check if there is a
-    discount, return a dict of all products with their old and new prices"""
+    discount, return a dict of all products with their old and new prices
+
+    for now, the function will use uoft's class names, this function will be
+    modified to take a list of class names of different websites to loop over
+    """
 
     discounts = {}
     for url in products:
 
         product = Product(url)
-        product.set_curr_price()
-        product.set_old_price()
-        print(product.curr_price, product.old_price)
+        product.set_curr_price('product-views-price-lead')
+        product.set_old_price('product-views-price-old')
 
         if product.old_price:
-            discounts[url] = [product.old_price, product.curr_price]
+            discounts[url] = product
 
     return discounts
 
 
-# all_p = get_all_products(WebScrapper(URL))
-# print(all_p)
+def export_csv(discounts: Dict[str, Product]) -> None:
+    """ Export the discounts into a CSV file"""
+
+    with open(PATH, 'a', newline='') as csvfile:
+        write = csv.writer(csvfile)
+
+        for product in discounts.keys():
+            write.writerow([product, discounts[product].old_price,
+                            discounts[product].curr_price])
+
+
 all_p = {'https://www.uoftbookstore.com/Jong-Erica-Fear-Of-Flying'}
 discount = check_discount(all_p)
+export_csv(discount)
 
 print(f'discounts {discount}')
